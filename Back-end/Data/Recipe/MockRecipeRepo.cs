b164aba78c06e435b09b5ef8a63363c38f6b0e53
sbecerek <suheybbecerek@gmail.com>
2021-03-23 00:35:51 +0300

    added mock data for recipe

--- Back-end/Data/Recipe/MockRecipeRepo.cs
+++ Back-end/Data/Recipe/MockRecipeRepo.cs
@@ -7,12 +7,36 @@ namespace Back_end.Data
     {
         public Recipe GetRecipeById(int id)
         {
-            throw new System.NotImplementedException();
+            return new Recipe{id=3,
+                            creatorId=3,
+                            name="Simit",
+                            description="Turkish bagel with sesame",
+                            rating=6.9F,
+                            tag="Turkish Cuisine"};
         }
 
         public IEnumerable<Recipe> GetRecipes()
         {
-            throw new System.NotImplementedException();
+                        return new List<Recipe>{
+                new Recipe{id=1,
+                            creatorId=1,
+                            name="Pilav",
+                            description="Boiled rice fried with butter",
+                            rating=9.8F,
+                            tag="Turkish Cuisine"},
+                new Recipe{id=2,
+                            creatorId=2,
+                            name="Karni Yarik",
+                            description="Eggplants stuffed with minced meat",
+                            rating=10.0F,
+                            tag="Turkish Cuisine"},
+                new Recipe{id=3,
+                            creatorId=3,
+                            name="Simit",
+                            description="Turkish bagel with sesame",
+                            rating=6.9F,
+                            tag="Turkish Cuisine"}
+            };
         }
     }
 }
