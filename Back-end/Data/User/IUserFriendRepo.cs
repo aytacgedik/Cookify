8703bb0eda7417acbeac41f246faa2e9a7da0d72
Onur Çağlayan <onurcag99@gmail.com>
2021-03-23 19:17:56 +0300

    Initial version of User and UserFriend controllers.

---
+++ Back-end/Data/User/IUserFriendRepo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Back_end.Models;
+
+namespace Back_end.Data
+{
+    public interface IUserFriendRepo
+    {
+        IEnumerable<UserFriend> AddUserFriendById(int _userFollowerId, int _userFollowedId);
+        IEnumerable<UserFriend> RemoveUserFriendById(int _userFollowerId, int _userFollowedId);
+    }
+}
