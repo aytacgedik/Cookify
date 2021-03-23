a7277e2c83a677accd82fd47e5c3f1245ffe39ad
sbecerek <suheybbecerek@gmail.com>
2021-03-23 19:40:15 +0300

    Revert "Merge pull request #17 from aytacgedik/Implementation-of-User-and-UserFriend-Controllers"
    This reverts commit d1bf125247e596e461a77b6e23ce26d1c5d16490, reversing
    changes made to c997ebedc4f4933af9335d1676d98fdda9473a9c.

--- Back-end/Data/User/IUserRepo.cs
+++ Back-end/Data/User/IUserRepo.cs
@@ -1,3 +1,4 @@
+//Creating this cs as example. Gonna be changed later on
 using System.Collections.Generic;
 using Back_end.Models;
 
@@ -5,10 +6,7 @@ namespace Back_end.Data
 {
     public interface IUserRepo
     {
-        IEnumerable<User> CreateUser(int id, string name, string surname, string email, bool verified, bool admin);
-        IEnumerable<User> RemoveUserById(int id);
-        User UpdateUserById(int id, string name, string surname, string email, bool verified, bool admin);
-        User GetUserById(int id);
         IEnumerable<User> GetUsers();
+        User GetUserById(int id);
     }
 }
