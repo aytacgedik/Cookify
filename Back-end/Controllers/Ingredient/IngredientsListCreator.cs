be24f4baef24b36111528c1108b0cab1d6fa4a3d
eraytuncyilmaz <eraytuncyilmaz@gmail.com>
2021-03-24 04:15:42 +0100

    Ingredient Controller implemented

---
+++ Back-end/Controllers/Ingredient/IngredientsListCreator.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Back_end.Data;
+using Back_end.Models;
+
+namespace Back_end.Controllers
+{
+    [Route("api/ingredients/list")]
+    [ApiController]
+    public class IngredientsListCreator : IngredientController
+    {
+        public IngredientsListCreator(IRecipeRepo recipeRepository, IIngredientRepo ingredientRepository) : base(recipeRepository, ingredientRepository)
+        {
+
+        }
+
+        [HttpGet]
+        public ActionResult generateList([FromQuery] int id)
+        {
+            List<Ingredient> ingredientsList = new List<Ingredient>();
+            foreach (var recipe in _recipeRepository.GetRecipes())
+            {
+                if (recipe.id == id)
+                {
+                    ingredientsList.Add(recipe.getRecipeIngredient());
+                }
+            }
+
+            return Ok(ingredientsList);
+        }
+    }
+}
---
+++ Back-end/Data/Ingredient/IIngredientRepo.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Back_end.Models;
+
+namespace Back_end.Data
+{
+    public interface IIngredientRepo
+    {
+        Ingredient GetIngredientById(int id);
+        IEnumerable<Ingredient> GetIngredients();
+    }
+}
