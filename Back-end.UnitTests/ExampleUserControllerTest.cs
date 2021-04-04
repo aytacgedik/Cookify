f763a0d4b9c1fa89bab79b25dafdb50894e32e8b
Aytac <aytacgedik20@gmail.com>
2021-04-04 16:58:28 +0300

    COOK-53-Implementation-of-DTO-Extensions-Hotfix

--- Back-end.UnitTests/ExampleUserControllerTest.cs
+++ Back-end.UnitTests/ExampleUserControllerTest.cs
@@ -21,7 +21,7 @@ namespace Back_end.UnitTests
             // Arrange
             var repositoryStub=new Mock<IUserRepo>();
 
-            repositoryStub.Setup(repo => repo.GetUsers()).Returns((IEnumerable<UserDto>)null);
+            repositoryStub.Setup(repo => repo.GetUsers()).Returns((IEnumerable<User>)null);
 
             var controller = new ExampleUserController(repositoryStub.Object);
 
