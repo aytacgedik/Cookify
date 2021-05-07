05a54a9eeb44e93f5ed65ab5dab1a8cda9f056ad
Aytac <aytacgedik20@gmail.com>
2021-05-07 16:56:57 +0300

    Recipe

--- Front-end/src/components/pages/Recipes.js
+++ Front-end/src/components/pages/Recipes.js
@@ -1,30 +1,18 @@
-import React, {useState, useEffect} from 'react';
+import React,{useState,useEffect} from 'react';
 import '../../App.css';
 import axios from 'axios';
-
+import RecipeCard from './RecipeCard'
 export default function Recipes() {
-  const [data, setData] = useState("");
-
+  const [cards,setCards] = useState([])
   useEffect(() => {
-    const fetchData = async () => {
-      const result = await axios.get(`https://cookify.azurewebsites.net/api/recipes`);
-      setData(result.data);
-    }
-    fetchData();
-
-  }, []);
-
-  if (data) {
-    return (
-      <ul className='recipes'>
-        {data.map((recipe, i) => {return <li key={i + 1}> Recipe nr {i + 1} :<br/> {recipe.name}<br/> {recipe.description}</li> })}      
-      </ul>
-      );
-    } else {
-      return (
-        <ul>
-
-        </ul>
-      )
-    }
+      axios.get('/api/recipes',
+      {headers: {"Access-Control-Allow-Origin": "*"},
+    responseType: 'json'})
+      .then(res =>
+        {
+            setCards(res.data);
+        })
+  }, [])
+  
+  return cards.map(card =><RecipeCard name={card.name} description={card.description} rating={card.rating} tag={card.tag}/>);
 }
