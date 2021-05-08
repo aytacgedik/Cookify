fb382520bc13167d7d1ffdc1a9f64b49ef905792
Onur Caglayan <62426634+onurmx@users.noreply.github.com>
2021-05-08 20:29:52 +0300

    Initial commit for implementation of COOK80.

---
+++ Back-end.IntegrationTests/UserControllerTests/UserFriendControllerTests.cs
@@ -0,0 +1,35 @@
+using System.Threading.Tasks;
+using Xunit;
+using FluentAssertions;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using Back_end.Models;
+using Back_end.Data;
+using Back_end.Dtos;
+
+namespace Back_end.IntegrationTests
+{
+    public class UserFriendControllerTests : IntegrationTest
+    {
+        [Fact]
+        public async Task createUserFriend_ReturnsOK()
+        {
+            await AuthenticateAsync();
+            var response = await TestClient.PostAsJsonAsync("api/user_friends/", new UserFriend
+            {
+                userFollowedId = 3,
+                userFollowerId = 5
+            });
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+
+        // public async Task removeUser_ReturnOK()
+        // {
+        //     await AuthenticateAsync();
+        //     var response = await TestClient.DeleteAsync("api/users/",,);
+        //     response.StatusCode.Should().Be(HttpStatusCode.OK);
+        // }
+    }
+}
