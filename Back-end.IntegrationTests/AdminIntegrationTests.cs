57d945e47efd05032eb3b7c3bef5d8657039ada3
sbecerek <suheybbecerek@gmail.com>
2021-05-09 05:52:02 +0300

    small-fix

--- Back-end.IntegrationTests/AdminIntegrationTests.cs
+++ Back-end.IntegrationTests/AdminIntegrationTests.cs
@@ -4,15 +4,7 @@ namespace Back_end.IntegrationTests
 {
     public class AdminIntegrationTests : IntegrationTest
     {
-        [Fact]
-        public void Get()
-        {
-        //Given
-        
-        //When
-        
-        //Then
-        }
+  
         
     }
 }
