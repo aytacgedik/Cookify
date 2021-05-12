7e2f61780658582708e6789640f540941bdcdf66
Aytac <aytacgedik20@gmail.com>
2021-05-12 19:30:36 +0300

    COOK-87-Ingredient

--- Front-end/src/components/pages/Ingredients.js
+++ Front-end/src/components/pages/Ingredients.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../../App.css';
-
+import axios from 'axios';
+import IngredientCard from './IngredientCard'
 export default function Ingredients() {
-  return <h1 className='ingredients'>INGREDIENTS</h1>;
+  const [cards, setCards] = useState([])
+  useEffect(() => {
+    axios.get('/api/ingredient',
+      {
+        headers: { "Access-Control-Allow-Origin": "*" },
+        responseType: 'json'
+      })
+      .then(res => {
+        setCards(res.data);
+      })
+  }, [])
+
+  return cards.map(card => <IngredientCard name={card.name} />);
 }
+
