160cbeeb1ccf829b6def91973204f2312160b116
Antoni Karpiński <antoni.karpinski@Antonis-MacBook-Pro.local>
2021-05-13 17:22:47 +0200

    Initial: fetch data and display.

--- Front-end/src/components/pages/Recipes.js
+++ Front-end/src/components/pages/Recipes.js
@@ -1,6 +1,30 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import '../../App.css';
+import axios from 'axios';
 
 export default function Recipes() {
-  return <h1 className='recipes'>RECIPES</h1>;
+  const [data, setData] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get(`https://cookify.azurewebsites.net/api/recipes`);
+      setData(result.data);
+    }
+    fetchData();
+
+  }, []);
+
+  if (data) {
+    return (
+      <ul className='recipes'>
+        {data.map((recipe, i) => {return <li key={i + 1}> Recipe nr {i + 1} :<br/> {recipe.name}<br/> {recipe.description}</li> })}      
+      </ul>
+      );
+    } else {
+      return (
+        <ul>
+
+        </ul>
+      )
+    }
 }
