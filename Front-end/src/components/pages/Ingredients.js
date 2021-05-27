7c3b924f35b192d545138b06bc7b6bd3ab53147c
Aytac <aytacgedik20@gmail.com>
2021-05-27 05:44:03 +0300

    Ingredient update

--- Front-end/src/components/pages/Ingredients.js
+++ Front-end/src/components/pages/Ingredients.js
@@ -3,8 +3,31 @@ import '../../App.css';
 import axios from 'axios';
 import IngredientCard from './IngredientCard'
 export default function Ingredients() {
+  const [text, setText] = useState("")
   const [cards, setCards] = useState([])
-  useEffect(() => {
+
+  const changeText = (e) => {
+    setText(e.target.value)
+  }
+  
+
+  var axiosConfig = {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+    responseType: 'json'
+  };
+  const SearchCard = () => {
+    axios.get("/api/ingredients/search", { params: { query: text } },
+      {
+        headers: { "Access-Control-Allow-Origin": "*" },
+        responseType: 'json'
+      })
+      .then(res => {
+        setCards(res.data);
+      })
+  }
+  const GetCards = () => {
     axios.get('/api/ingredient',
       {
         headers: { "Access-Control-Allow-Origin": "*" },
@@ -13,7 +36,15 @@ export default function Ingredients() {
       .then(res => {
         setCards(res.data);
       })
-  }, [])
-  return cards.map(card => <IngredientCard name={card.name} />);
+  }
+
+  return <div>
+    <input type="text" onChange={(e) => changeText(e)} />
+    <button className="button" onClick={SearchCard}>Search</button>
+    <button className="button" onClick={GetCards}>Get All Ingredient</button><br /><br />
+    <div>
+      {cards.map(card => <IngredientCard name={card.name} />)}
+    </div>
+  </div>;
 }
 
