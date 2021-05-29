2a43bb8815885057d7ef48e99ff717bfcd1789ca
eraytuncyilmaz <eraytuncyilmaz@gmail.com>
2021-05-29 21:34:58 +0100

    COOK-95 correction

--- Back-end/Services/Ingredient Services/IngredientServices.cs
+++ Back-end/Services/Ingredient Services/IngredientServices.cs
@@ -19,6 +19,8 @@ namespace Back_end.Services
         public IngredientDto GetIngredientById(int id)
         {
             var ingredient = _ingredientRepository.GetIngredientById(id);
+            if (ingredient == null)
+                return null;
             return ingredient.AsDto();
         }
 
