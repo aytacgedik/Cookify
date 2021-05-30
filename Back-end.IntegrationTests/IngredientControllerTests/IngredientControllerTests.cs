5f1a1cdf46fb706e55e81dd00fc04bd1e905d13c
eraytuncyilmaz <eraytuncyilmaz@gmail.com>
2021-05-30 05:01:29 +0100

    COOK-100 Updating Ingredient Integration Tests

--- Back-end.IntegrationTests/IngredientControllerTests/IngredientControllerTests.cs
+++ Back-end.IntegrationTests/IngredientControllerTests/IngredientControllerTests.cs
@@ -12,60 +12,60 @@ namespace Back_end.IntegrationTests
 {
     public class IngredientControllerTests : IntegrationTest
     {
-        // [Fact]
-        // public async Task GetIngredient_WithoutId_ReturnsOK()
-        // {
-        //     //Arrange
-        //     var mockRecipeRepo = new MockRecipeRepo();
-        //     await AuthenticateAsync();
+        [Fact]
+        public async Task GetIngredient_WithoutId_ReturnsOK()
+        {
+            //Arrange
+            //var mockRecipeRepo = new MockRecipeRepo();
+            await AuthenticateAsync();
 
-        //     //Act
-        //     var response = await TestClient.GetAsync("api/ingredient");
+            //Act
+            var response = await TestClient.GetAsync("api/ingredient");
 
-        //     //Assert
-        //     response.StatusCode.Should().Be(HttpStatusCode.OK);
-        // }
+            //Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
 
-        // [Fact]
-        // public async Task GetIngredint_WithId_ReturnsOK()
-        // {
-        //     await AuthenticateAsync();
-        //     var response = await TestClient.GetAsync("api/ingredient/1");
-        //     response.StatusCode.Should().Be(HttpStatusCode.OK);
-        // }
+        [Fact]
+        public async Task GetIngredint_WithId_ReturnsOK()
+        {
+            await AuthenticateAsync();
+            var response = await TestClient.GetAsync("api/ingredient/1");
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
 
-        // [Fact]
-        // public async Task searchIngredient_ValidQuery_ReturnsOK()
-        // {
-        //     await AuthenticateAsync();
-        //     var hersheyList = new List<Ingredient> { new Ingredient { id = 1, name="Hershey Dark Chocolate" } }.Select(x => x.AsDto());
-        //     var response = await TestClient.GetAsync("api/ingredients/search?query=Hershey Dark Chocolate");
-        //     response.StatusCode.Should().BeEquivalentTo(HttpStatusCode.OK);
-        //     var returnedResponse = response.Content.ReadFromJsonAsync<List<RecipeDto>>().Result;
-        //     returnedResponse.Should().BeEquivalentTo(hersheyList);
-        // }
+        [Fact]
+        public async Task searchIngredient_ValidQuery_ReturnsOK()
+        {
+            await AuthenticateAsync();
+            var hersheyList = new List<IngredientDto> { new IngredientDto { id = 1, name = "Hershey Dark Chocolate" } }.Select(x => x);
+            var response = await TestClient.GetAsync("api/ingredients/search?query=Hershey Dark Chocolate");
+            response.StatusCode.Should().BeEquivalentTo(HttpStatusCode.OK);
+            var returnedResponse = response.Content.ReadFromJsonAsync<List<RecipeDto>>().Result;
+            returnedResponse.Should().BeEquivalentTo(hersheyList);
+        }
 
-        // [Fact]
-        // public async Task searchIngredients_ValidQuery_ReturnsOK()
-        // {
-        //     await AuthenticateAsync();
-        //     var chocolateList = new List<Ingredient> { new Ingredient { id = 1, name = "Hershey Dark Chocolate" }, 
-        //         new Ingredient { id = 2, name = "Cadbury Milky Chocolate" }, 
-        //         new Ingredient { id = 3, name = "Lindt White Chocolate" } }.Select(x => x.AsDto());
-        //     var response = await TestClient.GetAsync("api/ingredients/search?query=Chocolate");
-        //     response.StatusCode.Should().BeEquivalentTo(HttpStatusCode.OK);
-        //     var returnedResponse = response.Content.ReadFromJsonAsync<List<RecipeDto>>().Result;
-        //     returnedResponse.Should().BeEquivalentTo(chocolateList);
-        // }
+        [Fact]
+        public async Task searchIngredients_ValidQuery_ReturnsOK()
+        {
+            await AuthenticateAsync();
+            var chocolateList = new List<IngredientDto> { new IngredientDto { id = 1, name = "Hershey Dark Chocolate" },
+                 new IngredientDto { id = 2, name = "Cadbury Milky Chocolate" },
+                 new IngredientDto { id = 3, name = "Lindt White Chocolate" } }.Select(x => x);
+            var response = await TestClient.GetAsync("api/ingredients/search?query=Chocolate");
+            response.StatusCode.Should().BeEquivalentTo(HttpStatusCode.OK);
+            var returnedResponse = response.Content.ReadFromJsonAsync<List<RecipeDto>>().Result;
+            returnedResponse.Should().BeEquivalentTo(chocolateList);
+        }
 
-        // /*[Fact]
-        // public async Task searchIngredient_InvalidQuery_ReturnsNotFound()
-        // {
-        //     await AuthenticateAsync();
-        //     var response = await TestClient.GetAsync("api/ingredients/search?query=hebele");
-        //     response.StatusCode.Should().BeEquivalentTo(HttpStatusCode.NotFound);
-        //     // var returnedResponse = response.Content.ReadFromJsonAsync<List<RecipeDto>>();
-        //     // returnedResponse.Result.Should().BeEquivalentTo(null);
-        // }*/
+        [Fact]
+        public async Task searchIngredient_InvalidQuery_ReturnsNotFound()
+        {
+            await AuthenticateAsync();
+            var response = await TestClient.GetAsync("api/ingredients/search?query=hebele");
+            response.StatusCode.Should().BeEquivalentTo(HttpStatusCode.OK);
+            // var returnedResponse = response.Content.ReadFromJsonAsync<List<RecipeDto>>();
+            // returnedResponse.Result.Should().BeEquivalentTo(null);
+        }
     }
 }
