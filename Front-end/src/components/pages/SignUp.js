6d83b4805f4f5eb7e7029e256b768d8bcc8e259b
Aytac <aytacgedik20@gmail.com>
2021-06-17 18:31:19 +0300

    front-end-hotfix-4

--- Front-end/src/components/pages/SignUp.js
+++ Front-end/src/components/pages/SignUp.js
@@ -1,40 +1,70 @@
-import React, { Component } from "react";
+import React, { Component,useState } from "react";
 import '../../index.css'
 import '../../App.css'
 import './SignInUp.css'
+import axios from 'axios';
+
+
+var id =0;
+var name ="";
+var surname ="";
+var email ="";
+var registered =false;
+
+
+var axiosConfig = {
+    headers: {
+        "Access-Control-Allow-Origin" : "*",
+    },
+    responseType : 'json'
+};
+const SendCard = () => {
+    console.log(name)
+    axios.post('/api/users', {
+      "id":Math.floor(Math.random() * 10000),
+      "name": name,
+      "surname": surname,
+      "email":email,
+      "verified":true,
+      "admin":true
+    },axiosConfig).then(res => {
+      console.log(res)
+      registered = true;
+    });
+    
+  }
+
 export default class SignUp extends Component {
     render() {
+        
         return (
             <div className="auth-wrapper">
             <div className="auth-inner">
-            <form>
-                <h3>Sign Up</h3>
-
+            <h3>Sign Up</h3>
                 <div className="cookify-form-group">
                     <label>First name</label>
-                    <input type="text" className="cookify-form-control" placeholder="First name" />
+                    <input type="text" id="fname" onChange={(e) => name=e.target.value} className="cookify-form-control" placeholder="First name" />
                 </div>
 
                 <div className="cookify-form-group">
                     <label>Last name</label>
-                    <input type="text" className="cookify-form-control" placeholder="Last name" />
+                    <input type="text" id="lname" onChange={(e) => surname=e.target.value} className="cookify-form-control" placeholder="Last name" />
                 </div>
 
                 <div className="cookify-form-group">
                     <label>Email address</label>
-                    <input type="email" className="cookify-form-control" placeholder="Enter email" />
+                    <input type="email" id="email" onChange={(e) => email=e.target.value} className="cookify-form-control" placeholder="Enter email" />
                 </div>
 
                 <div className="cookify-form-group">
                     <label>Password</label>
-                    <input type="password" className="cookify-form-control" placeholder="Enter password" />
+                    <input type="password" id="password" className="cookify-form-control" placeholder="Enter password" />
                 </div>
 
-                <button type="submit" className="cookify-btn cookify-btn-primary cookify-btn-block">Sign Up</button>
+                <button type="submit" onClick={(SendCard)} className="cookify-btn cookify-btn-primary cookify-btn-block">Sign Up</button>
                 <p className="forgot-password text-right">
                     Already registered <a href="/sign-in">sign in?</a>
                 </p>
-            </form>
             </div>
             </div>
         );
